61da5071e33ddee60d7d1b7fb9db59f911da1acb
Cem <31999641+cemoso@users.noreply.github.com>
2020-05-25 13:51:15 +0100

    Changed logo and removed github logo and socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="NACS"
+            style={{ height: '10em' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
